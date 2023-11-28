bc6224b3c0263a31c5624a53fe29e5e6dafd2427
suraiyasultana826 <suraiyasultana826@gmail.com>
2023-11-29 00:35:54 +0600

    dashboard

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
 
 
         //user related api
+
+        app.get('/users', async(req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+
         app.post('/users', async(req, res) => {
             const user = req.body;
             const query = {email: user.email}
