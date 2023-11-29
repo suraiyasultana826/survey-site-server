6bf60e50185dde82e164ab82575d387a38e0373c
suraiyasultana826 <suraiyasultana826@gmail.com>
2023-11-29 13:15:21 +0600

    installed stripe

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); 
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -84,6 +85,21 @@ async function run() {
             console.log(completed);
             const result = await surveyCompletionCollection.insertOne(completed);
             res.send(result);
+        });
+
+        //payment intent
+        app.post('/create-payment-intent', async(req, res) => {
+            const {price} = req.body;
+            const amount = parseInt(price * 100);
+            console.log(amount);
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
         })
 
         // Send a ping to confirm a successful connection
