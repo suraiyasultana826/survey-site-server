e9975b71880883924668e2b07870904feb73e46d
suraiyasultana826 <suraiyasultana826@gmail.com>
2023-11-30 19:58:48 +0600

    added jwt token

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -33,6 +34,15 @@ async function run() {
         const surveyCompletionCollection = client.db('surveySite').collection("completed");
         const paymentCollection = client.db('surveySite').collection("payments");
 
+        //jwt related api
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send({ token });
+        })
         app.get('/surveys', async (req, res) => {
             const cursor = surveyCollection.find();
             const result = await cursor.toArray();
@@ -40,20 +50,64 @@ async function run() {
         })
 
 
+        //middlewares
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+        //use verify token after verifyToken
+
+        const verifyAdmin = async(req, res, next)=>{
+            const email = req.decoded.email;
+            const query = {email: email};
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if(!isAdmin){
+                return res.status(403).send({ message: 'forbidden access'});
+            }
+            next();
+        }
+
+
         //user related api
 
-        app.get('/users', async(req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            console.log(req.headers);
             const result = await userCollection.find().toArray();
             res.send(result);
         });
 
+        app.get('/user/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'unauthorized access' })
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
 
-        app.post('/users', async(req, res) => {
+
+        app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email}
+            const query = { email: user.email }
             const existingUser = await userCollection.findOne(query);
-            if(existingUser){
-                return res.send({message: 'user already exists', insertedId: null})
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
             }
             const result = await userCollection.insertOne(user);
             res.send(result);
@@ -61,7 +115,7 @@ async function run() {
 
         //created survey
 
-        app.post('/surveys', async(req, res) => {
+        app.post('/surveys', async (req, res) => {
             const created = req.body;
             console.log(created);
             const result = await surveyCollection.insertOne(created);
@@ -73,7 +127,7 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const options = {
 
-                projection: { image:1, category:1, voted:1, description:1, questionOne:1, questionTwo:1, questionThree:1, title: 1, imdb: 1 },
+                projection: { image: 1, category: 1, voted: 1, description: 1, questionOne: 1, questionTwo: 1, questionThree: 1, title: 1, imdb: 1 },
             };
             const result = await surveyCollection.findOne(query, options);
             res.send(result);
@@ -81,7 +135,7 @@ async function run() {
 
         //completedSurvey
 
-        app.post('/completed', async(req, res) => {
+        app.post('/completed', async (req, res) => {
             const completed = req.body;
             console.log(completed);
             const result = await surveyCompletionCollection.insertOne(completed);
@@ -89,8 +143,8 @@ async function run() {
         });
 
         //payment intent
-        app.post('/create-payment-intent', async(req, res) => {
-            const {price} = req.body;
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
             const amount = parseInt(price * 100);
             console.log(amount);
             const paymentIntent = await stripe.paymentIntents.create({
@@ -104,11 +158,23 @@ async function run() {
         })
 
 
-        app.post('/payments', async(req, res) => {
+        app.post('/payments', async (req, res) => {
             const payment = req.body;
             const paymentResult = await paymentCollection.insertOne(payment);
             console.log('payment info', payment);
             res.send(paymentResult);
+        });
+
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
